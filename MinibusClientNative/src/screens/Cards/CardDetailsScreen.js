85d759539f96f96459a99310f67242318216b8ec
Morgan <morganitos@hotmail.com>
2019-11-02 23:39:38 +0100

    improve card details screen

--- MinibusClientNative/src/screens/Cards/CardDetailsScreen.js
+++ MinibusClientNative/src/screens/Cards/CardDetailsScreen.js
@@ -14,7 +14,18 @@ class CardDetailsScreen extends React.Component {
         return (
             <SafeAreaView>
                 <Text h3>Details</Text>
-                <Text>{this.runId}</Text>
+                {this.props.runList.map( (item) => {
+                    item.id == this.runId && console.log(item)
+                    return (
+                    item.id == this.runId && (
+                    <View key={item.id}>
+                        <Text>Enfant : {item.child}</Text>
+                        <Text>Type : {item.type}</Text>
+                        <Text>Date : {item.timestamp}</Text>
+                        
+                    </View>
+                    ))
+                })}
             </SafeAreaView>
         )
     }
@@ -29,3 +40,8 @@ const mapDispatchToProps = (dispatch, props) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CardDetailsScreen);
+
+// <Text>Distance : {item.run.totalDist / 1000}km</Text>
+//                         <Text>Temps : {(this.run.summary.totalTime / 3600).toFixed(0)}h{((this.summary.totalTime / 60) % 60).toFixed(0)}min</Text>
+//                         <Text>Péage : {this.summary.tollCost.car}€</Text>
+//                         <Text>Coût carburant : {this.summary.consumption}€</Text>
